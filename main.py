e9a0a780a8a9b85321cd5709275a93e414fd512a
Ulises Lopez <98845522+Ucddnuts@users.noreply.github.com>
2023-01-09 17:56:38 +0000

    nothing

---
+++ main.py
@@ -0,0 +1,9 @@
+#vocab
+#putting a # in front of something makes it a comment
+#a comment is not proccesed
+#an IDE is a integrated development enviorment
+# a prepository in github is like a binder in a library. a repository is a folder where you can save out program files
+#camel case is a notation standard used accross programing languages
+x=7
+y=10
+print (x+y)
